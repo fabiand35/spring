8f07ed2d0fc36f64f28b2291eac17154ade4583b
Davis Schnebelt <davis-schnebelt@web.de>
2023-01-12 19:50:30 +0100

    Testing

--- src/test/java/org/openapitools/configuration/TestSecurityConfiguration.java
+++ src/test/java/org/openapitools/configuration/TestSecurityConfiguration.java
@@ -9,6 +9,7 @@ import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.HttpStatusEntryPoint;
 
 @Configuration
+@Profile("test")
 @EnableWebSecurity
 public class TestSecurityConfiguration {
 
