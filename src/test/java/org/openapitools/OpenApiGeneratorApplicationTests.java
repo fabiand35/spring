d1e984c0bb53f61a4e9407977fe4d38db9b8ec26
Davis Schnebelt <davis-schnebelt@web.de>
2023-01-12 19:44:27 +0100

    Testing

--- src/test/java/org/openapitools/OpenApiGeneratorApplicationTests.java
+++ src/test/java/org/openapitools/OpenApiGeneratorApplicationTests.java
@@ -1,13 +1,26 @@
 package org.openapitools;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
-class OpenApiGeneratorApplicationTests {
+@AutoConfigureMockMvc()
+class PersonalV1ApiBackendApplicationTests {
+
+    @Autowired
+    private MockMvc mockMvc;
 
     @Test
-    void contextLoads() {
+    void testStatusApiCall() throws Exception {
+        //Setup Mock Behavior
+        this.mockMvc.perform(get("/personnel/status/"))
+                .andExpect(status().isOk());
     }
 
 }
