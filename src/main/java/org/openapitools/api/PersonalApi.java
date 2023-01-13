5bfa34965e5e5e30f45d0e548ee640468bb61fd2
Fabian Droll <fadr@arku.com>
2023-01-13 10:51:32 +0100

    testing

--- src/main/java/org/openapitools/api/PersonalApi.java
+++ src/main/java/org/openapitools/api/PersonalApi.java
@@ -14,7 +14,6 @@ import org.openapitools.model.PersonalStatusGet200Response;
 import java.util.UUID;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
-import io.swagger.v3.oas.annotations.Parameters;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
@@ -26,12 +25,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.context.request.NativeWebRequest;
-import org.springframework.web.multipart.MultipartFile;
 
 import javax.validation.Valid;
-import javax.validation.constraints.*;
-import java.util.List;
-import java.util.Map;
 import java.util.Optional;
 import javax.annotation.Generated;
 
@@ -44,7 +39,6 @@ public interface PersonalApi {
     default Optional<NativeWebRequest> getRequest() {
         return Optional.empty();
     }
-
     /**
      * GET /personnel/assignments/ : get all personal assignments
      *
